d65f0dd7ef4ebc5a8e3b23ae1f805241f222c3a5
Koustubh <koustubh.code@gmail.com>
2022-06-28 00:47:17 +0530

    intital commit

---
+++ go-rest-api-server.go
@@ -0,0 +1,19 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/test", test)
+
+	http.ListenAndServe(":5000", router)
+}
+
+func test(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("This is a test"))
+}
