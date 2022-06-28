fc07c836cfb8efa5666f20dddd3e0fe7b70502c5
Koustubh <koustubh.code@gmail.com>
2022-06-29 00:34:58 +0530

    added route-handlers

--- go-rest-api-server.go
+++ go-rest-api-server.go
@@ -1,19 +1,37 @@
-package api
+package main
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+var data []string = []string{}
+
 func main() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/test", test)
+	router.HandleFunc("/add/{item}", addItem).Methods("GET", "DELETE")
 
 	http.ListenAndServe(":5000", router)
 }
 
 func test(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("This is a test"))
+	w.Header().Set("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(struct {
+		ID string
+	}{ID: "555"})
+}
+
+func addItem(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	routeVar := mux.Vars(r)["item"]
+
+	data = append(data, routeVar)
+
+	json.NewEncoder(w).Encode(data)
 }
